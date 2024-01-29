5bf79ecd85b4db357b205658acf201a08850183f
任帅 <1064425721@qq.com>
2024-01-29 17:21:25 +0800

    feat: compatible `jsconfig.json`

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,38 @@ import { join } from 'path';
 import { existsSync, readFileSync } from 'fs';
 import stripJsonComments from 'strip-json-comments';
 
-const filePath = join(process.cwd(), 'tsconfig.json');
-
 const alias = () => {
-  const fileExists = existsSync(filePath);
-  if (!fileExists) {
-    return [{ find: '@', replacement: join(process.cwd(), 'src') }]
+  const tsconfigPath = join(process.cwd(), 'tsconfig.json');
+  const jsconfigPath = join(process.cwd(), 'jsconfig.json');
+
+  const tsconfigExists = existsSync(tsconfigPath);
+  const jsconfigExists = existsSync(jsconfigPath);
+
+  if (!tsconfigExists && !jsconfigExists) {
+    console.warn('tsconfig.json or jsconfig.json not found');
   };
 
-  const tsConfigStr = readFileSync(filePath, 'utf8');
-  const tsConfig = JSON.parse(stripJsonComments(tsConfigStr.replace(/,\s*([\]}])/g, '$1')));
-  const paths: TsConfigPaths = tsConfig?.compilerOptions?.paths;
-  if (!paths) {
-    return [{ find: '@', replacement: join(process.cwd(), 'src') }]
+  let configStr = '';
+  if (tsconfigExists) {
+    configStr = readFileSync(tsconfigPath, 'utf8');
+  } else {
+    configStr = readFileSync(jsconfigPath, 'utf8');
   };
+  const tsConfig = JSON.parse(stripJsonComments(configStr.replace(/,\s*([\]}])/g, '$1')));
+  const paths: TsConfigPaths = tsConfig?.compilerOptions?.paths;
+
+  if (!paths) {
+    console.warn("tsconfig.json's paths or jsconfig.json's paths not found");
+  };
+
+  // todo: 未处理 tsconfig.json 中的 baseUrl
+  // todo: 判断数据格式是否正确
 
   const alias: AliasArr = [];
   Object.entries(paths).forEach(([key, value]) => {
     alias.push(({ find: key?.replace(/\/\*$/, ''), replacement: join(process.cwd(), value[0]?.replace(/\/\*$/, '')) }));
   });
-  
+
   return alias;
 };
 
